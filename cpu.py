75c4ce9a811982cb8b73d4963498fa6fadcf2275
liyue <liyue@agora.io>
2019-04-10 18:52:41 +0800

    add load_avg

--- cpu.py
+++ cpu.py
@@ -17,6 +17,9 @@ def getProcess(pName):
 
     return process_lst
 
+def load_avg():
+    f = os.popen("uptime | sed 's/,//g' | awk '{print $8,$9,$10}'")
+    return f.read().strip()
 # 获取进程名位Python的进程对象列表
 process_lst = getProcess("htop")
 
@@ -32,12 +35,21 @@ process_lst = getProcess("htop")
 # for process_instance in process_lst:
 #     print(process_instance.cpu_percent(None))
 
-sys_mem = psutil.virtual_memory().percent
-# 系统内存使用
-print('system mem usage is ',sys_mem)
+# sys_mem = psutil.virtual_memory().percent
+# # 系统内存使用
+# print('system mem usage is ',sys_mem)
 
-# 系统CPU利用率
+# # 系统CPU利用率
 psutil.cpu_percent(None)
 
-time.sleep(3)
-print('system cpu usage is: ', psutil.cpu_percent(None))
+# time.sleep(3)
+# print('system cpu usage is: ', psutil.cpu_percent(None))
+
+# load_avg = os.getloadavg()
+
+while True:
+    sys_mem = psutil.virtual_memory().percent
+    print('system mem usage is: ',sys_mem)
+    print('system cpu usage is: ', psutil.cpu_percent(None))
+    print('system load average is:', load_avg(), '\n')
+    time.sleep(2)
