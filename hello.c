b0758929e84c883851049e574b0c76262576e8f0
liyue <liyue@agora.io>
2019-05-20 12:17:58 +0800

    update perf

---
+++ hello.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+ 
+// int main()
+// {
+//     /* 我的第一个 C 程序 */
+//     printf("Hello, World! \n");
+ 
+//     return 0;
+// }
+#include <stdio.h>
+
+int main()
+{
+    printf("hello,world! \n");
+    return 0;
+}
