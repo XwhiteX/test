ebb5e278b2eb33ea483ab6d1023fe2bcd9b0ffaf
liyue <liyue@agora.io>
2019-08-29 21:12:30 +0800

    translate txt to csv

--- test/plotly.py
+++ test/plotly.py
@@ -1,9 +1,9 @@
 def line_plots(name="line_plots.html"):
     dataset = {
         'cpu': [],
-        'memory' : [],
-        'load' : [],
-        'online' : []
+        'memory': [],
+        'load': [],
+        'online': []
     }
     with open('/Users/yueli/Desktop/linux_share/2.8.0_stable_perf/perf/com/health_check_case1_cpp_mix_com.txt') as f:
         i = 0
@@ -16,5 +16,6 @@ def line_plots(name="line_plots.html"):
     x = dataset['cpu']
     y = dataset['memory']
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     line_plots
--- test/txt.py
+++ test/txt.py
@@ -1,5 +1,27 @@
-with open('/Users/yueli/Desktop/linux_share/2.8.0_stable_perf/perf/com/health_check_case1_cpp_mix_com.txt') as f:
-    i = 0
-    for line in f:
-        iterms = line.split()
-        print(iterms)
+import csv
+import re
+
+
+def modify_text():
+    with open('/Users/yueli/Desktop/linux_share/2.8.0_stable_perf/perf/com/health_check_case1_cpp_mix_com.txt',
+              "r+", ) as f:
+        read_data = f.read()
+        read_data_no_special = re.sub('[{}[\]\'-]', '', read_data)
+        f.seek(0)
+        f.write(read_data_no_special.replace(':', ','))
+
+
+modify_text()
+
+if __name__ == '__main__':
+    inputFile = '/Users/yueli/Desktop/linux_share/2.8.0_stable_perf/perf/com/health_check_case1_cpp_mix_com.txt'
+    outputFile = '/Users/yueli/Desktop/linux_share/2.8.0_stable_perf/perf/com/health_check_case1_cpp_mix_com.csv'
+    row = []
+
+    csvFile = open(outputFile, 'w', newline='')
+    writer = csv.writer(csvFile)
+    writer.writerow(row)
+    lines = open(inputFile, 'r', encoding='utf-8', ).readlines()
+    for line in lines:
+        csvFile.write(line)
+    csvFile.close()
