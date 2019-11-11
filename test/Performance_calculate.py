fed1e6372838480714ea074d8a944b09b4416197
liyue <liyue@agora.io>
2019-11-11 13:51:29 +0800

    update

--- test/Performance_calculate.py
+++ test/Performance_calculate.py
@@ -1,21 +1,11 @@
 # -*- coding:utf-8 -*-
 import re
+import sys
 
-# file_path = '/Users/kkk/Desktop/health_check_case1_cpp_single_live.txt'
 checklist = ['cpu_percent', 'percent', 'bps_rx', 'min1', 'min5', 'min15'] 
+# file_path = '/Users/yueli/Desktop/health_check_case4_cpp_single_live_h265.txt' # TODO 自动匹配地址
+file_path = sys.argv[1]
 
-file_path = '/Users/yueli/Desktop/health_check_case4_cpp_single_live_h265.txt' # TODO 自动匹配地址
-
-# def calculat(check_op):
-#     with open(file_path, 'r') as f:
-#         data_list = []
-#         for line in f.readlines():
-#             list_a = line.strip().split(",") # 把数据按照逗号分隔开
-#             list_b = [x.strip() for x in list_a] # 把列表中的前后空格和换行符去掉
-#             if check_op in list_b:
-#                 data = list_b.index(check_op) + 1
-#                 data_list.append(list_b[data])
-#         return data_list
 def calculat(check_op):
     with open(file_path, 'r') as f:
         data_list = []
@@ -27,6 +17,7 @@ def calculat(check_op):
                 data = list_b.index(check_op) + 1
                 data_list.append(list_b[data])
         del(data_list[-2:]) # 去除list最后两个数
+        del(data_list[:2])  # 去除list前面两个数
         # print(data_list)
         return data_list
 
@@ -36,26 +27,22 @@ def avg(check_list):
         for item in calculat(check):
             sum_data += float(item)
         avg_data = '%.2f' % (sum_data / float(len(calculat(check)))) # 四舍五入取后两位
-
         if check == 'cpu_percent':
             print('performance machine\'s CPU use percent is: ' + str(avg_data) + '%\n')
         elif check == 'percent':
             print('performance machine\'s MEM use percent is: ' + str(avg_data) + '%\n')
         elif check == 'bps_rx': # 带宽换算还需要重新计算，目前拿到的是bps_rx
-            print('performance machine\'s NETWORK use is: ' + str(avg_data) + '\n')
+            print('performance machine\'s NETWORK use is: ' + str(avg_data) + 'bps_rx\n')
         else:
-            print('performance machine\'s system load average ' + check + 'is: ' + str(avg_data) + '\n')
+            print('performance machine\'s system load average ' + check + ' is: ' + str(avg_data) + '\n')
 
-avg(checklist)
-# calculat(check_op='cpu_percent')
+if __name__ == "__main__":
+    avg(checklist)
+    # calculat(check_op='cpu_percent')
+    # calculat(check_op='cpu_percent')
 
 
-        # a = calculat(check)
-        # sum_data = 0
-        # for item in a:
-        #     sum_data += float(item)
-        # avg_data = sum_data / float(len(a))
-        # print('performance machine\'s ' + check + ' is:\n' + str(avg_data) + '\n')
+
 
 
 
